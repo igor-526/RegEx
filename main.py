1ce68862399beaac46632b8a0fa20e2f3cf77c0a
Igor Ter-Pogosyan <igor-526@yandex.ru>
2023-02-09 19:32:54 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from pprint import pprint
+import re
+# читаем адресную книгу в формате CSV в список contacts_list
+import csv
+with open("phonebook_raw.csv") as f:
+  rows = csv.reader(f, delimiter=",")
+  contacts_list = list(rows)
+pprint(contacts_list)
